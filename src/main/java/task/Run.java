8a72058f54079093ff0482a0d155cce228c7913e
yarmail <yarmail@yandex.ru>
2023-03-07 23:29:09 +0300

    Начало работы на проектом, подготовительные работы

---
+++ src/main/java/task/Run.java
@@ -0,0 +1,16 @@
+package task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Run {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);
+        System.out.println("---------------");
+        System.out.println("проект запущен");
+        System.out.println("---------------");
+        System.out.println("Go to http://localhost:8080/");
+    }
+}
