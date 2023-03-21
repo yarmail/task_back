c6486badeffae3009d9c3fa2b851ea44faa7747d
yarmail <yarmail@yandex.ru>
2023-03-21 15:54:57 +0300

    update Run

--- src/main/java/task/Run.java
+++ src/main/java/task/Run.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Run {
-
     public static void main(String[] args) {
         SpringApplication.run(Run.class, args);
         System.out.println("---------------");
